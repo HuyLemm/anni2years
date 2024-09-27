3ba3f7d33ec2f8cb251e9ccd776ae1536043502a
HuyLemm <lthuy171103@gmail.com>
2024-09-28 00:23:07 +0700

    update: confirm idea and finish 1st step of idea

--- anniversary-website/src/pages/Intro2.js
+++ anniversary-website/src/pages/Intro2.js
@@ -1,41 +1,178 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
 import racing from '../assets/images/intro2/racing.png';
 import road from '../assets/images/intro2/road.png';
+import ankwypolice from '../assets/images/intro2/ankwypolice.png';
+import police from '../assets/images/intro2/police.png';
+import greenlight from '../assets/images/intro2/greenlight.png';
+import redlight from '../assets/images/intro2/redlight.png';
+import motor from '../assets/images/intro2/motor.png';
+import motorpolice from '../assets/images/intro2/motorpolice.png';
+import square from '../assets/images/intro2/square.png';
 
 
 const Intro2 = () => {
+  const [light, setLight] = useState('green'); 
+  const [carPosition, setCarPosition] = useState('start'); 
+  
   useEffect(() => {
-    // Bạn có thể thêm các xử lý liên quan hoặc âm thanh tại đây nếu cần.
+    const timer = setTimeout(() => {
+      setLight('red');
+    }, 2000);
+
+    return () => clearTimeout(timer); 
   }, []);
 
   return (
-    <div className="h-screen flex items-center justify-center relative" style={{ overflow: 'hidden' }}>
-      {/* Road 2 Image */}
+    <div className="h-screen flex items-center  relative" style={{ overflow: 'hidden' }}>
+      {/* Road Image */}
       <img
         src={road}
         alt="Road"
-        className="absolute"
+        className="absolute center"
         style={{
-          top: '100px', // Tùy chỉnh vị trí top cho road2
-          left: '100px', // Tùy chỉnh vị trí left cho road2
-          width: '600px', // Tùy chỉnh kích thước cho road2
+          left: '-530px',
+          width: '1500px', 
           zIndex: 1,
         }}
       />
 
-      {/* Racing 2 Image */}
       <img
-        src={racing}
-        alt="Racing "
-        className="absolute"
+        src={road}
+        alt="Road"
+        className="absolute center"
         style={{
-          top: '200px', // Tùy chỉnh vị trí top cho racing2
-          left: '150px', // Tùy chỉnh vị trí left cho racing2
-          width: '150px', // Tùy chỉnh kích thước cho racing2
+          right: '-510px',
+          width: '1500px', 
+          zIndex: 1,
+        }}
+      />
+
+      <img
+        src={road}
+        alt="Road"
+        className="absolute center"
+        style={{
+          transform: 'rotate(90deg)',
+          left: '130px',
+          width: '1500px', 
+          zIndex: 1,
+        }}
+      />
+
+      <img
+        src={square}
+        alt="square"
+        className="absolute center"
+        style={{
+          left:'800px',
+          width: '200px', 
+          zIndex: 1,
+        }}
+      />
+
+      {/* Racing Image */}
+      <motion.img
+        src={racing}
+        alt="Racing"
+        className="absolute"
+        initial={{ x: '-100px' }} 
+        animate={{
+          x: 600,
+        }}
+        transition={{
+          duration: 4, 
+          ease: 'easeInOut', 
+        }}
+        onAnimationComplete={() => setCarPosition('nearLight')} 
+        style={{
+          top: '350px',
+          width: '150px',
           zIndex: 2,
         }}
       />
+
+      {/* Police Image */}
+      <img
+        src={police}
+        alt="police "
+        className="absolute"
+        style={{
+          top: '250px',
+          right: '880px', 
+          width: '50px', 
+          zIndex: 2,
+        }}
+      />
+
+      <img
+        src={ankwypolice}
+        alt="police "
+        className="absolute"
+        style={{
+          top: '240px',
+          right: '880px', 
+          width: '65px', 
+          zIndex: 1,
+        }}
+      />
+      {/* Motor Image */}
+      <img
+        src={motor}
+        alt="motor "
+        className="absolute"
+        style={{
+          top: '280px',
+          right: '760px', 
+          width: '120px', 
+          zIndex: 1,
+        }}
+      />
+
+      <img
+        src={motorpolice}
+        alt="motorpolice"
+        className="absolute"
+        style={{
+          top: '330px',
+          right: '820px', 
+          width: '150px', 
+          zIndex: 1,
+        }}
+      />
+
+      {/* Light Image */}
+      {light === 'green' ? (
+        <img
+          src={greenlight}
+          alt="Green Light"
+          className="absolute"
+          style={{
+            top: '220px',
+            left: '750px',
+            width: '65px',
+            zIndex: 1,
+          }}
+        />
+      ) : (
+        <img
+          src={redlight}
+          alt="Red Light"
+          className="absolute"
+          style={{
+            top: '220px',
+            left: '750px',
+            width: '65px',
+            zIndex: 1,
+          }}
+        />
+      )}
+
+
     </div>
+
+    
   );
 };
 
