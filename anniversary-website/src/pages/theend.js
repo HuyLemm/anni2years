b92d3a26043c4b887713af0ffed720643b2f05a0
HuyLemm <lthuy171103@gmail.com>
2024-12-17 11:24:23 +0700

    update

--- anniversary-website/src/pages/theend.js
+++ anniversary-website/src/pages/theend.js
@@ -25,7 +25,7 @@ const TheEnd = () => {
     "6. Dạo chơi trung tâm thương mại chill chill 🛍️",
   ];
 
-  const imageUrl = "https://i.imgur.com/6r9yNyl.png";
+  const imageUrl = "https://i.imgur.com/d2HSM7v.png";
 
   useEffect(() => {
     // Hiển thị tiêu đề sau 1 giây
